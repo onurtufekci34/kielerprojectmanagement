0f2a4614f196a79f9bbfccdab51c88b47bafdb76
onurtufekci34 <onurtufekci61@gmail.com>
2023-12-14 21:00:20 +0100

    firebase connection

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3oyykPrd61Ea6rZWuq8POx-FqKYKsge0",
+  authDomain: "react-chatapp-30766.firebaseapp.com",
+  projectId: "react-chatapp-30766",
+  storageBucket: "react-chatapp-30766.appspot.com",
+  messagingSenderId: "891314053253",
+  appId: "1:891314053253:web:290ec476b73f84e80089ec",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
