159a5aa4e3c2e1757249aebfc00ebc5236ad70df
onurtufekci34 <onurtufekci61@gmail.com>
2023-12-20 18:22:02 +0100

    signup configuration

--- src/hooks/useSignup.jsx
+++ src/hooks/useSignup.jsx
@@ -1,8 +1,9 @@
 
 
 import { useState } from "react"
-import {auth} from '../firebase/config'
+import {auth,storage} from '../firebase/config'
 import {createUserWithEmailAndPassword,updateProfile } from 'firebase/auth'
+import {ref,uploadBytes,getDownloadURL} from 'firebase/storage'
 import {useAuthContext} from './useAuthContext'
 
 export const useSignup=()=>{
@@ -11,21 +12,28 @@ export const useSignup=()=>{
     const [isPending,setIsPending]=useState(false)
     const {dispatch}=useAuthContext();
 
-    const signup=async (email,password,userName)=>{
+    const signup=async (email,password,userName,thumbnail)=>{
         setError(null)
         setIsPending(true)
 
         try {
 
             const response=await createUserWithEmailAndPassword(auth,email,password)
-            console.log(response.user);
+            //console.log(response.user);
 
             if(!response){
                 throw new Error('Üyelik işlemi gerçekleşemedi')
             }
 
+            const filePath = `thumbnails/${response.user.uid}/${thumbnail.name}`
+            const storageRef = ref(storage,filePath)
+            await uploadBytes(storageRef,thumbnail)
+
+            const imgUrl = await getDownloadURL(storageRef)
+
             updateProfile(response.user,{
-                displayName:userName
+                displayName:userName,
+                photoUrl:imgUrl
             })
 
             dispatch({type:'LOGIN',payload:response.user})
