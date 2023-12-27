0a2c39bb346bfad3fc978e7e6c47dbf252009cf4
onurtufekci34 <onurtufekci61@gmail.com>
2023-12-27 17:11:38 +0100

    login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,38 @@
 import './Login.css'
+import {useState} from 'react'
+
+
+
+
+
 
 export default function Login() {
+
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+   
+    
+  }
+
+
   return (
-    <div>Login</div>
+    <form className='auth-form' onSubmit={handleSubmit}>
+    <h2>Login Page</h2>
+    <label>
+      <span>Email:</span>
+      <input type="email" required value={email} onChange={(e)=>setEmail(e.target.value)} />
+    </label>
+    <label>
+      <span>Password:</span>
+      <input type="password" required value={password} onChange={(e)=>setPassword(e.target.value)} />
+    </label>
+    
+    <button className="btn">Login</button>
+  </form>
   )
 }
+
