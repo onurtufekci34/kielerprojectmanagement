11c8482110ae4368cb6436dbff535cbc2c6b2c49
onurtufekci34 <onurtufekci61@gmail.com>
2024-01-03 21:43:13 +0100

    navbarlinks up to user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,42 @@
-import './Navbar.css'
-import {Link} from 'react-router-dom'
-import Logo1 from '../assets/logo1.png'
-import {useLogout} from '../hooks/useLogout'
-
-
-
-
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Logo1 from "../assets/logo1.png";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-
-    const {logout} =useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
-    <div className='navbar'>
-    <ul>
-        <li className='logo'>
-        <img src={Logo1} alt="Kieler Project Management" />
-            <span>Kieler Project Management</span>
+    <div className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={Logo1} alt="Kieler Project Management" />
+          <span>Kieler Project Management</span>
         </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
 
-        <li>
-            <Link to="/login">Login</Link>
-        </li>
-        <li>
-            <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-            <button className='logout-btn' onClick={logout}>Logout</button>
-        </li>
-    </ul>
-</div>
-  )
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="logout-btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && <button disabled className="logout-btn">Logging out!</button>}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
 }
