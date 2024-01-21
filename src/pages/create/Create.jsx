d14d7af38a72735947a7736abb06292573a07f21
onurtufekci34 <onurtufekci61@gmail.com>
2024-01-21 14:09:15 +0100

    project object created

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,90 +1,136 @@
-import './Create.css'
-import { useState,useEffect } from 'react'
-import Select from 'react-select'
-import {useCollection} from '../../hooks/useCollection'
+import "./Create.css";
+import { useState, useEffect } from "react";
+import Select from "react-select";
+import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 export default function Create() {
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [date, setDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [projectUsers, setProjectUsers] = useState([]);
+  const [userss, setUserss] = useState([]);
+  const [formError, setFormError] = useState(null);
 
-  const [name,setName]=useState('')
-  const [details,setDetails]=useState('')
-  const [date,setDate]=useState('')
-  const [category,setCategory]=useState('')
-  const [projectUsers,setProjectUsers]=useState([])
-  const [user,setUser] = useState([])
-  const [formError,setFormError] = useState(null)
-
+  const { user } = useAuthContext();
 
   const categories = [
-    {value:'desktop', label:'Desktop App'},
-    {value:'web', label:'Web App'},
-    {value:'mobile', label:'Mobile App'}
-  ]
+    { value: "desktop", label: "Desktop App" },
+    { value: "web", label: "Web App" },
+    { value: "mobile", label: "Mobile App" },
+  ];
 
-  const {documents} = useCollection('users')
+  const { documents } = useCollection("users");
   //console.log(documents)
 
-  const handleSubmit=(e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setFormError(null)
+    setFormError(null);
 
-    if(!category){
-      setFormError('Please select a category')
-      return
+    if (!category) {
+      setFormError("Please select a category");
+      return;
     }
 
-    if(!projectUsers.length<1){
-      setFormError('Please select a user')
-      return
+    if (projectUsers.length < 1) {
+      setFormError("Please select a user");
+      return;
     }
-    
-    console.log(name,details,date,category,projectUsers);
-  }
 
-  useEffect(()=>{
-    if(documents){
-      const options = documents.map(user=>{
-        return {value:user,label:user.usersName}
-      })
+    const creator = {
+      usersName: user.displayName,
+      fotoUrl: user.photoURL,
+      id: user.uid,
+    };
 
-      setUser(options)
+    const projectUsersList = projectUsers.map((u) => {
+      return {
+        userName: u.value.usersName,
+        fotoUrl: u.value.fotoUrl,
+        id: u.value.id,
+      };
+    });
+
+    const newProject = {
+      isim: name,
+      explanation: details,
+      kategori: category.value,
+      finishDate: new Date(date),
+      comments: [],
+      creator,
+      projectUsersList,
+    };
+
+    console.log(newProject);
+  };
+
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.usersName };
+      });
+
+      setUserss(options);
     }
-  },[documents])
+  }, [documents]);
 
   return (
-    <div className='create-form'>
-      <h2 className='page-title'>Create New Project</h2>
+    <div className="create-form">
+      <h2 className="page-title">Create New Project</h2>
       <form onSubmit={handleSubmit}>
         <label>
           <span>Project Name:</span>
-          <input required type="text" onChange={(e)=>setName(e.target.value)} value={name}
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
         </label>
 
         <label>
           <span>Project Description:</span>
-          <textarea required type="text" onChange={(e)=>setDetails(e.target.value)} value={details}
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
           ></textarea>
         </label>
 
         <label>
           <span>End Date:</span>
-          <input required type="date" onChange={(e)=>setDate(e.target.value)} value={date}
+          <input
+            required
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
           />
         </label>
 
         <label>
           <span>Category:</span>
-          <Select placeholder="Select" options={categories} onChange={(option) => setCategory(option)}/>
+          <Select
+            placeholder="Select"
+            options={categories}
+            onChange={(option) => setCategory(option)}
+          />
         </label>
 
         <label>
           <span>Project Users:</span>
-          <Select placeholder='Select Project User' options={user} onChange={(option) => setProjectUsers(option)} isMulti/>
+          <Select
+            placeholder="Select Project User"
+            options={userss}
+            onChange={(option) => setProjectUsers(option)}
+            isMulti
+          />
         </label>
 
-        <button className='btn'>Add Project</button>
-        {formError && <div className='error'>{formError}</div>}
+        <button className="btn">Add Project</button>
+        {formError && <div className="error">{formError}</div>}
       </form>
     </div>
-  )
+  );
 }
